3c31d330380070c9bfa63e7cb723c8bcb1565ceb
SHAJ <shakilahmedshaj@gmail.com>
2018-10-21 18:57:15 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/android_file_data_store/MainActivity.java
@@ -0,0 +1,13 @@
+package com.t3ch.shaj.android_file_data_store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
